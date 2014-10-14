af46e66cd33e20e0bffeae6b9a5f6c0bf633507f
Cindy <cindyofwu@gmail.com>
2014-10-13 20:17:45 -0400

    some confusing things i did that work but very slow

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 from flask import Flask, render_template, request
 from bs4 import BeautifulSoup
 
+import search
+
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("input.html")
-
-@app.route("/answer")
-def answer():
-   question= request.args.get("question_input")
-   print question
-   #need to figure out which file to send it to
-   answer= search.find(question)
-   return render_template("output.html", question = question, answer= answer) 
-
+@app.route("/", methods = ["GET","POST"])
+@app.route("/error", methods = ["GET","POST"])
+def home(error = None):
+    if (request.method == "POST"):
+        return redirect(url_for('answer'))
+        return render_template("input.html")
+        
+@app.route("/answer", methods = ["GET","POST"])
+def answer(question = None, answer = None, error = None):
+    question = request.form["question_input"]
+    if ("who" not in question.lower() and "when" not in question.lower()):
+        return render_template("input.html", error = "Error: Invalid question. Must contain who or when.")
+    s = search.find(question)
+    answer = s
+    return render_template("output.html", question=question, answer=answer)
 
 
 if __name__ == "__main__":
